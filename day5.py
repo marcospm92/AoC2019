7ea35ed041f21e8bda1dff661eb05ec43ae28b47
Marcos Perez Martin <marcospm92@gmail.com>
2019-12-11 16:40:28 +0100

    Day 5

---
+++ day5.py
@@ -0,0 +1,241 @@
+opcode = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 89, 49, 225,
+          1102, 35, 88, 224, 101, -3080, 224, 224, 4, 224, 102, 8, 223, 223,
+          1001, 224, 3, 224, 1, 223, 224, 223, 1101, 25, 33, 224, 1001, 224,
+          -58, 224, 4, 224, 102, 8, 223, 223, 101, 5, 224, 224, 1, 223, 224,
+          223, 1102, 78, 23, 225, 1, 165, 169, 224, 101, -80, 224, 224, 4, 224,
+          102, 8, 223, 223, 101, 7, 224, 224, 1, 224, 223, 223, 101, 55, 173,
+          224, 1001, 224, -65, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 1,
+          224, 1, 223, 224, 223, 2, 161, 14, 224, 101, -3528, 224, 224, 4, 224,
+          1002, 223, 8, 223, 1001, 224, 7, 224, 1, 224, 223, 223, 1002, 61, 54,
+          224, 1001, 224, -4212, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 1,
+          224, 1, 223, 224, 223, 1101, 14, 71, 225, 1101, 85, 17, 225, 1102,
+          72, 50, 225, 1102, 9, 69, 225, 1102, 71, 53, 225, 1101, 10, 27, 225,
+          1001, 158, 34, 224, 101, -51, 224, 224, 4, 224, 102, 8, 223, 223,
+          101, 6, 224, 224, 1, 223, 224, 223, 102, 9, 154, 224, 101, -639, 224,
+          224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1, 224, 223, 223, 4,
+          223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0,
+          99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0,
+          256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999,
+          1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105,
+          1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1,
+          99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0,
+          0, 106, 0, 0, 1105, 1, 99999, 108, 226, 226, 224, 102, 2, 223, 223,
+          1006, 224, 329, 101, 1, 223, 223, 1007, 677, 677, 224, 1002, 223, 2,
+          223, 1005, 224, 344, 1001, 223, 1, 223, 8, 226, 677, 224, 1002, 223,
+          2, 223, 1006, 224, 359, 1001, 223, 1, 223, 108, 226, 677, 224, 1002,
+          223, 2, 223, 1005, 224, 374, 1001, 223, 1, 223, 107, 226, 677, 224,
+          102, 2, 223, 223, 1006, 224, 389, 101, 1, 223, 223, 1107, 226, 226,
+          224, 1002, 223, 2, 223, 1005, 224, 404, 1001, 223, 1, 223, 1107, 677,
+          226, 224, 102, 2, 223, 223, 1005, 224, 419, 101, 1, 223, 223, 1007,
+          226, 226, 224, 102, 2, 223, 223, 1006, 224, 434, 1001, 223, 1, 223,
+          1108, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 449, 101, 1, 223,
+          223, 1008, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 464, 101, 1,
+          223, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 479, 101,
+          1, 223, 223, 1008, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 494,
+          101, 1, 223, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1005, 224,
+          509, 1001, 223, 1, 223, 1108, 226, 226, 224, 1002, 223, 2, 223, 1006,
+          224, 524, 101, 1, 223, 223, 7, 226, 226, 224, 102, 2, 223, 223, 1006,
+          224, 539, 1001, 223, 1, 223, 107, 226, 226, 224, 102, 2, 223, 223,
+          1006, 224, 554, 101, 1, 223, 223, 107, 677, 677, 224, 102, 2, 223,
+          223, 1006, 224, 569, 101, 1, 223, 223, 1008, 677, 677, 224, 1002,
+          223, 2, 223, 1006, 224, 584, 1001, 223, 1, 223, 8, 677, 226, 224,
+          1002, 223, 2, 223, 1005, 224, 599, 101, 1, 223, 223, 1108, 226, 677,
+          224, 1002, 223, 2, 223, 1005, 224, 614, 101, 1, 223, 223, 108, 677,
+          677, 224, 102, 2, 223, 223, 1005, 224, 629, 1001, 223, 1, 223, 8,
+          677, 677, 224, 1002, 223, 2, 223, 1005, 224, 644, 1001, 223, 1, 223,
+          7, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 659, 1001, 223, 1,
+          223, 1007, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 674, 101, 1,
+          223, 223, 4, 223, 99, 226]
+
+
+def run(index, opcode):
+    while 1:
+        print("\nIndex: " + str(index))
+        print("Full OpCode: " + str(opcode[index]))
+
+        code = int(str(opcode[index])[-2:])
+        print("Opcode: " + str(code))
+
+        opcode_length = len(str(opcode[index]))
+        print("Length: " + str(opcode_length))
+
+        if code == 1 or code == 2 or code == 7 or code == 8:
+            print("Params: " + str(opcode[index + 1]) + ", " +
+                  str(opcode[index + 2]) + ", " + str(opcode[index + 3]))
+        elif code == 3 or code == 4:
+            print("Param: " + str(opcode[index + 1]))
+        elif code == 5 or code == 6:
+            print("Params: " + str(opcode[index + 1]) + ", " +
+                  str(opcode[index + 2]))
+        elif code == 99:
+            print("No param. End of program")
+
+        mode_p1 = 0
+        mode_p2 = 0
+        mode_p3 = 0
+        if opcode_length >= 3:
+            mode_p1 = int(str(opcode[index])[-3])
+        if opcode_length >= 4:
+            mode_p2 = int(str(opcode[index])[-4])
+        if opcode_length >= 5:
+            mode_p3 = int(str(opcode[index])[-5])
+        print("Mode of param 1: " + str(mode_p1))
+
+        if code != 3 and code != 4:
+            print("Mode of param 2: " + str(mode_p2))
+            if code != 5 and code != 6:
+                print("Mode of param 3: " + str(mode_p3))
+
+        if code == 1 or code == 2:
+            if code == 1:
+                print("\nSUM")
+            else:
+                print("\nMULTIPLY")
+
+            dig1 = 0
+            dig2 = 0
+            dest = 0
+            if mode_p1 == 0:
+                dig1 = int(opcode[opcode[index + 1]])
+                print("Digit 1: opcode[" + str(opcode[index + 1]) +
+                      "] = " + str(dig1))
+            else:
+                dig1 = int(opcode[index + 1])
+                print("Digit 1: " + str(dig1))
+
+            if mode_p2 == 0:
+                dig2 = int(opcode[opcode[index + 2]])
+                print("Digit 2: opcode[" + str(opcode[index + 2]) +
+                      "] = " + str(dig2))
+            else:
+                dig2 = int(opcode[index + 2])
+                print("Digit 2: " + str(dig2))
+
+            if mode_p3 == 0:
+                dest = int(opcode[opcode[index + 3]])
+                print("Destination: opcode[" + str(opcode[index + 3]) + "]." +
+                      " Actual value: " + str(opcode[opcode[index + 3]]))
+                if code == 1:
+                    opcode[opcode[index + 3]] = dig1 + dig2
+                else:
+                    opcode[opcode[index + 3]] = dig1 * dig2
+                print("Final value: " + str(opcode[opcode[index + 3]]))
+            else:
+                dest = opcode[index + 3]
+                print("Destination: " + str(dest))
+                if code == 1:
+                    opcode[index + 3] = dig1 + dig2
+                else:
+                    opcode[index + 3] = dig1 * dig2
+                print("Final value: " + str(opcode[index + 3]))
+            index = index + 4
+
+        elif code == 3:
+            print("\nINPUT")
+            print("Place to save: opcode[" + str(opcode[index + 1]) + "]." +
+                  " Actual value: " + str(opcode[opcode[index + 1]]))
+            opcode[opcode[index + 1]] = int(input())
+            index = index + 2
+
+        elif code == 4:
+            print("\nOUTPUT")
+            if mode_p1 == 0:
+                print("pos '" + str(index+1) + "' (opcode[" +
+                      str(opcode[index + 1]) + "]: " +
+                      str(opcode[opcode[index + 1]]) + ")")
+            else:
+                print(opcode[index + 1])
+            index = index + 2
+
+        elif code == 5:
+            print("\nJUMP IF TRUE")
+            if mode_p1 == 0:
+                print(str(opcode[opcode[index + 1]]) + " & 0 are distinct?")
+                dig1 = int(opcode[opcode[index + 1]])
+            else:
+                print(str(opcode[index + 1]) + " & 0 are distinct?")
+                dig1 = int(opcode[index + 1])
+
+            if int(dig1) != 0:
+                print("True")
+                if mode_p2 == 0:
+                    index = int(opcode[opcode[index + 2]])
+                else:
+                    index = int(opcode[index + 2])
+            else:
+                print("False")
+                index = index + 3
+
+        elif code == 6:
+            print("\nJUMP IF FALSE")
+            if mode_p1 == 0:
+                print(str(opcode[opcode[index + 1]]) + " & 0 are equal?")
+                dig1 = int(opcode[opcode[index + 1]])
+            else:
+                print(str(opcode[index + 1]) + " & 0 are equal?")
+                dig1 = int(opcode[index + 1])
+
+            if int(dig1) == 0:
+                print("True")
+                if mode_p2 == 0:
+                    index = int(opcode[opcode[index + 2]])
+                else:
+                    index = int(opcode[index + 2])
+            else:
+                print("False")
+                index = index + 3
+
+        elif code == 7:
+            print("\nLESS THAN")
+            if mode_p1 == 0:
+                dig1 = int(opcode[opcode[index + 1]])
+            else:
+                dig1 = int(opcode[index + 1])
+            if mode_p2 == 0:
+                dig2 = int(opcode[opcode[index + 2]])
+            else:
+                dig2 = int(opcode[index + 2])
+            dig3 = int(opcode[index + 3])
+
+            print(str(dig1) + " < " + str(dig2) + "?")
+            if dig1 < dig2:
+                print("True. Opcode[" + str(dig3) + "] = 1")
+                opcode[dig3] = 1
+            else:
+                print("False. Opcode[" + str(dig3) + "] = 0")
+                opcode[dig3] = 0
+            index = index + 4
+
+        elif code == 8:
+            print("\nEQUALS")
+            if mode_p1 == 0:
+                dig1 = int(opcode[opcode[index + 1]])
+            else:
+                dig1 = int(opcode[index + 1])
+            if mode_p2 == 0:
+                dig2 = int(opcode[opcode[index + 2]])
+            else:
+                dig2 = int(opcode[index + 2])
+            dig3 = int(opcode[index + 3])
+            print(str(dig1) + " & " + str(dig2))
+            if dig1 == dig2:
+                print("True. Opcode[" + str(dig3) + "] = 1")
+                opcode[dig3] = 1
+            else:
+                print("False. Opcode[" + str(dig3) + "] = 0")
+                opcode[dig3] = 0
+            index = index + 4
+
+        elif code == 99:
+            print("\nEND")
+            break
+        else:
+            print("\nERROR")
+            break
+
+
+print("PART 1")
+index = 0
+print(opcode)
+run(index, opcode)
+print(opcode)
